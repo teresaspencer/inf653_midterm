a8d0644e1511e386fcadfde859733247c01f4716
Teresa Spencer <teresalandkamer@gmail.com>
2026-03-17 08:48:28 -0500

    updated read_single

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -12,3 +12,25 @@
 
     // Instantiate author object
     $author = new Author($db);
+
+    // Get ID
+    $author->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    // Get author
+    $result = $author->read_single();
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+
+    // Create array
+    if($row) {
+        $author->id = $row['id'];
+        $author->author = $row['author'];
+        
+        $author_arr = array(
+            'id' => $author->id,
+            'author' => $author->author
+        );
+        // Make JSON
+        echo json_encode($author_arr);
+    } else {
+        echo json_encode(array('message' => 'No Author Found'));
+    }
--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -12,3 +12,25 @@
 
     // Instantiate Category object
     $category = new Category($db);
+
+    // Get ID
+    $category->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    // Get category
+    $result = $category->read_single();
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+
+    // Create array
+    if($row) {
+        $category->id = $row['id'];
+        $category->category = $row['category'];
+        
+        $category_arr = array(
+            'id' => $category->id,
+            'category' => $category->category
+        );
+        // Make JSON
+        echo json_encode($category_arr);
+    } else {
+        echo json_encode(array('message' => 'No Category Found'));
+    }
