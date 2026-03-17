3a8bdfc9b41bad3cd232d1e19e41e617fe303836
Teresa Spencer <teresalandkamer@gmail.com>
2026-03-17 09:08:58 -0500

    finished delete files

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -12,3 +12,16 @@
 
     // Instantiate author object
     $author = new Author($db);
+
+    $data = json_decode(file_get_contents('php://input'));
+    // or: $author->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    // set ID to update
+    $author->id = $data->id;
+
+    // Delete Author
+    if($author->delete()) {
+        echo json_encode(array('message' => 'Author Deleted'));
+    } else {
+        echo json_encode(array('message' => 'Author Not Deleted'));
+    }
--- api/categories/delete.php
+++ api/categories/delete.php
@@ -12,3 +12,16 @@
 
     // Instantiate Category object
     $category = new Category($db);
+
+    $data = json_decode(file_get_contents('php://input'));
+    // or: $category->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    // set ID to update
+    $category->id = $data->id;
+
+    // Delete Category
+    if($category->delete()) {
+        echo json_encode(array('message' => 'Category Deleted'));
+    } else {
+        echo json_encode(array('message' => 'Category Not Deleted'));
+    }
