08cb6783bcaae07fcc12533d196786b541a16400
Teresa Spencer <teresalandkamer@gmail.com>
2026-03-17 09:12:45 -0500

    updated create files

--- api/authors/create.php
+++ api/authors/create.php
@@ -12,3 +12,19 @@
 
     // Instantiate author object
     $author = new Author($db);
+
+    // Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+    $author->author = $data->author;
+    $author->id = $data->id;
+
+    // Create Author
+    if($author->create()) {
+        echo json_encode(
+            array('message' => 'Author Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Author Not Created')
+        );
+    }
--- api/categories/create.php
+++ api/categories/create.php
@@ -12,3 +12,19 @@
 
     // Instantiate Category object
     $category = new Category($db);
+
+    // Get raw data
+    $data = json_decode(file_get_contents('php://input'));
+    $category->category = $data->category;
+    $category->id = $data->id;
+
+    // Create Category
+    if($category->create()) {
+        echo json_encode(
+            array('message' => 'Category Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Category Not Created')
+        );
+    }
